87b95da37633ae85e2a4e4113ac203fb3d9c4b07
middleclicker <60602265+middleclicker@users.noreply.github.com>
2023-01-19 15:15:43 +0800

    Code Cleanup

--- Scraper.py
+++ Scraper.py
@@ -15,6 +15,13 @@ from pywinauto.application import Application
 # User Settings
 MAX_PYQ = 1
 SCRAPER_NAME = "Middleclicker"
+REFRESH_DELAY = 1
+UPDATE_FREQ = 5
+SCROLL_DIST = 5
+
+# TODO: I dont know if wechat has bot detection...
+RANDOMIZED_SCROLL = False
+RANDOMIZED_REFRESH_DELAY = False
 
 HOSTNAME = ""
 DATABASE = "Posts"
@@ -24,6 +31,9 @@ PORT_ID = 5432
 conn = None
 cur = None
 
+# Helper Classes
+class GetOutOfLoop( Exception ):
+    pass
 
 # Helper Functions
 def DFS(win, layers):
@@ -50,7 +60,6 @@ def replace_emoji(text):
 
 
 def calc_time(input):
-    # NOTE: the number of seconds since 1970-01-01
     now = int(math.floor(datetime.now().timestamp()))
     if "刚刚" in input:
         return datetime.fromtimestamp(now).strftime("20%y-%m-%d")
@@ -88,35 +97,25 @@ def process_raw_media(raw_media):
     else:  # NA
         return 0
 
-
-# Hook into WeChat's process
-PID = 0
-for proc in psutil.process_iter():
-    try:
-        pinfo = proc.as_dict(attrs=['pid', 'name'])
-    except psutil.NoSuchProcess:
-        pass
-    else:
-        if 'WeChat.exe' == pinfo['name']:
-            PID = pinfo['pid']
-
-app = Application(backend='uia').connect(process=PID)
-
-all_pyq = []
-all_pyq_contents = set()
-
-last_content_cnt = 0
-has_updated = False
-
-start_date = datetime.now().strftime("20%y-%m-%d")
-
-class GetOutOfLoop( Exception ):
-    pass
-
 try:
+    # ------------Wechat PID Hook------------
+    PID = 0
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name'])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            if 'WeChat.exe' == pinfo['name']:
+                PID = pinfo['pid']
+
+    app = Application(backend='uia').connect(process=PID)
+
+    # ------------Database------------
     conn = psycopg2.connect(host=HOSTNAME, dbname=DATABASE, user=USERNAME, port=PORT_ID)
     cur = conn.cursor()
 
+    # Create data storage table
     build_table = ''' CREATE TABLE IF NOT EXISTS posts
     (
         uuid        int PRIMARY KEY NOT NULL,
@@ -132,72 +131,124 @@ try:
         contributor  text NOT NULL,
         count       int   
     );'''
-    insert_script = "INSERT INTO posts (uuid, name, content, media, date, likes, like_count, comments, comment_count, scraped_date, contributor, count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
-
+    insert_script = ''' INSERT INTO posts 
+    (
+        uuid, 
+        name, 
+        content, 
+        media, 
+        date, 
+        likes, 
+        like_count, 
+        comments, 
+        comment_count, 
+        scraped_date, 
+        contributor, 
+        count
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'''
     cur.execute(build_table)
 
-    sql = 'SELECT COUNT(*) FROM posts;'
+    # Get number of posts
     data = []
-    cur.execute(sql, data)
+    cur.execute('SELECT COUNT(*) FROM posts;', data)
     results = cur.fetchone()
     total_posts = results[0]
 
+    all_pyq = []
+    pyq_uuids = set()
+
+    has_updated = False
+    last_content_cnt = 0
+
+    # ------------Data Collection------------
     print("Collecting Data")
     while True:
-        # 如果按Esc关闭朋友圈页面，这里就会崩掉然后结束
         try:
             pyq_win = app['朋友圈']
         except:
             break
         try:
             pyqs = pyq_win.wrapper_object().descendants(depth=4)
-            for x in pyqs:
+            for pyq in pyqs:
                 try:
                     pyq_info = []
-                    classname = x.friendly_class_name()
-                    if (classname == "ListItem"):
-                        # 这是一条朋友圈
-                        processed_pyq = replace_emoji(x.window_text()).split('\n')
-                        author = processed_pyq[0]
-                        pyq_contents = "".join(processed_pyq[1:len(processed_pyq) - 3])
-                        # 这他妈是啥。。。
-                        if "包含" in processed_pyq[1] or "视频" in processed_pyq[1]:  # 没有文案，只有图片 or 视频
-                            media = processed_pyq[1]
-                            time = processed_pyq[2]
-                            pyq_contents = "NA"
-                        elif "包含" in processed_pyq[len(processed_pyq) - 3] or "视频" in processed_pyq[
-                            len(processed_pyq) - 3]:
-                            media = processed_pyq[len(processed_pyq) - 3]
-                            time = processed_pyq[len(processed_pyq) - 2]
-                        elif processed_pyq[len(processed_pyq) - 2] == "Tape":
-                            media = "Tape"
-                            time = processed_pyq[len(processed_pyq) - 3]
-                        elif "包含" in processed_pyq[len(processed_pyq) - 4] or "视频" in processed_pyq[
-                            len(processed_pyq) - 4]:  # 有文案，图片，地点
-                            media = processed_pyq[len(processed_pyq) - 4]
-                            time = processed_pyq[len(processed_pyq) - 2]
-                            pyq_contents = "".join(processed_pyq[1:len(processed_pyq) - 4])
-                        else:
-                            media = "NA"
-                            time = processed_pyq[len(processed_pyq) - 2]
+                    if (pyq.friendly_class_name() == "ListItem"): # Post detected
+                        processed_pyq = replace_emoji(pyq.window_text()).split('\n')
 
-                        if (pyq_contents in all_pyq_contents):
-                            # 已经爬过这一条了
+                        lineCount = len(processed_pyq)
+
+                        author = processed_pyq[0]
+                        pyq_contents = "".join(processed_pyq[1:lineCount - 3])
+                        
+                        if "包含" in processed_pyq[lineCount - 3] or "视频" in processed_pyq[lineCount - 3]:
+                            # 文案：      有/无
+                            # 图片 / 视频：有
+                            # 地点：      无
+                            # 嵌入链接：   无
+
+                            # 文案：有
+                            # ['middleclicker', 'abc', 'efg', '包含2张图片 / 视频', '1分钟前', '']
+                            # ['middleclicker', 'abc', '包含2张图片 / 视频', '1分钟前', '']
+                            media = processed_pyq[lineCount - 3]
+                            time = processed_pyq[lineCount - 2]
+
+                            # 文案：无
+                            # ['middleclicker', '包含2张图片 / 视频', '1分钟前', '']
+                            if "包含" in processed_pyq[1] or "视频" in processed_pyq[1]:
+                                pyq_contents = "NA"
+                        elif "包含" in processed_pyq[lineCount - 4] or "视频" in processed_pyq[lineCount - 4]:
+                            # 文案：      有/无
+                            # 图片 / 视频：有
+                            # 地点：      有
+                            # 嵌入链接：   无
+
+                            # 文案：有
+                            # ['middleclicker', 'abc', 'efg', '包含2张图片 / 视频', '深圳国际交流监狱', '1分钟前', '']
+                            media = processed_pyq[lineCount - 4]
+                            time = processed_pyq[lineCount - 2]
+
+                            # 文案：无
+                            # ['middleclicker', '包含2张图片 / 视频', '深圳国际交流监狱', '1分钟前', '']
+                            if "包含" in processed_pyq[1] or "视频" in processed_pyq[1]:
+                                pyq_contents = "NA"
+                        elif "前" in processed_pyq[lineCount - 3]:
+                            # 文案：      有/无
+                            # 图片 / 视频：无
+                            # 地点：      有/无，无所谓，还没记录地点
+                            # 嵌入链接：   有
+
+                            # 文案：有
+                            # ['middleclicker', 'abc', '来点问题！', '1分钟前', 'Tape / QQMusic / Whatever', '']
+                            # ['middleclicker', 'abc', '来点问题！', '深圳国际交流监狱', '1分钟前', 'Tape / QQMusic / Whatever', '']
+                            media = processed_pyq[lineCount - 2]
+                            time = processed_pyq[lineCount - 3]
+
+                            # 文案：无
+                            # ['middleclicker', '来点问题！', '1分钟前', 'Tape / QQ Music / Whatever', '']
+                            if "包含" in processed_pyq[2] or "视频" in processed_pyq[2]:
+                                pyq_contents = "NA"
+                        else:
+                            # 文案：      有
+                            # 图片 / 视频：无
+                            # 地点：      有/无
+                            # 嵌入链接：   无
+                            # ['middleclicker', 'abc', 'efg', '1分钟前', '']
+                            media = "NA"
+                            time = processed_pyq[lineCount - 2]
+
+                        uuid_time = processed_time.split('-')[0] + ":" + processed_time.split('-')[1] + ":" + \
+                                    processed_time.split('-')[2]
+                        uuid = generate_uuid(pyq_contents, author, uuid_time)
+
+                        if (uuid in pyq_uuids):
                             last_content_cnt += 1
                             continue
-                        last_content_cnt = 0
-                        all_pyq_contents.add(pyq_contents)
-
+                        pyq_uuids.add(pyq_contents)
                         processed_time = calc_time(time)
                         if processed_time == -1:
                             raise GetOutOfLoop
 
-                        print(pyq_contents, media, processed_time)
-
-                        uuid_time = processed_time.split('-')[0] + ":" + processed_time.split('-')[1] + ":" + \
-                                    processed_time.split('-')[2]
-
-                        pyq_info.append(generate_uuid(pyq_contents, author, uuid_time))
+                        pyq_info.append(uuid)
                         pyq_info.append(author)
                         pyq_info.append(pyq_contents)
                         pyq_info.append(process_raw_media(media))
@@ -206,14 +257,17 @@ try:
                         try:
                             hasComment = False
                             hasLikes = False
-                            edits = DFS(x, 6)
+
+                            edits = DFS(pyq, 6)
                             for e in edits:
                                 if (e.friendly_class_name() == "Edit"):
                                     hasLikes = True
+
                                     likes = replace_emoji(e.window_text())
                                 if (e.friendly_class_name() == "ListBox"):
-                                    pinglun = []
                                     hasComment = True
+
+                                    pinglun = []
                                     comments = e.children()
                                     for com in comments:
                                         if (com.friendly_class_name() == "ListItem"):
@@ -238,7 +292,7 @@ try:
                         pyq_info.append(SCRAPER_NAME)
 
                         total_posts += 1
-                        pyq_info.append(total_posts) # Dont question it, idk lol
+                        pyq_info.append(total_posts) # Until I figure out how to use grafana properly
 
                         all_pyq.append(pyq_info)
                 except Exception as e:
@@ -249,25 +303,26 @@ try:
         except Exception:
             pass
 
-        if (len(all_pyq) < MAX_PYQ):
-            # 向下滚动
-            cords = pyq_win.rectangle()
-            pywinauto.mouse.scroll(wheel_dist=-5, coords=(cords.left + 10, cords.bottom - 10))
-        elif len(all_pyq) == MAX_PYQ and not has_updated:
+        post_count = len(all_pyq)
+        refresh = (pyq_win.rectangle().left+50, pyq_win.rectangle().top+10)
+        scroll = (pyq_win.rectangle().left+10, pyq_win.rectangle().bottom-10)
+        if post_count < MAX_PYQ: # Scroll Down
+            pywinauto.mouse.scroll(wheel_dist=-SCROLL_DIST, coords=scroll)
+        elif post_count == MAX_PYQ and not has_updated:
             print("Finished Data Collection, entering monitoring mode")
-            for e in all_pyq[len(all_pyq) - 10:len(all_pyq)]:
+            for e in all_pyq[post_count - UPDATE_FREQ:post_count]:
                 try:
                     cur.execute(insert_script, e)
-                except psycopg2.IntegrityError:
+                except psycopg2.IntegrityError: # Ignore duplicated key error
                     conn.rollback()
                 else:
                     conn.commit()
         else:
-            t.sleep(1)
-            cords = pyq_win.rectangle()
-            mouse.click(coords=(cords.left + 50, cords.top + 10))  # Refresh
-        if len(all_pyq) % 10 == 0:
-            for e in all_pyq[len(all_pyq)-10:len(all_pyq)]:
+            # Active monitoring mode
+            t.sleep(REFRESH_DELAY)
+            mouse.click(coords=refresh)  # Refresh
+        if post_count % UPDATE_FREQ == 0:
+            for e in all_pyq[post_count-10:post_count]:
                 try:
                     cur.execute(insert_script, e)
                 except psycopg2.IntegrityError:
@@ -281,26 +336,3 @@ finally:
         cur.close()
     if conn is not None:
         conn.close()
-
-# CSV Mode
-# unsuccessful = []
-# filename = start_time + ".csv"
-# with open(filename, 'w', newline='') as file:
-#    header = ['uuid', 'author', 'content', 'media', 'time', 'likes', 'comments', 'scraped_date', 'contributor']
-#    writer = csv.DictWriter(file, fieldnames = header)
-#    writer.writeheader()
-#    for pyq in all_pyq:
-#        try:
-#            writer.writerow(pyq)
-#        except:
-#            unsuccessful.append(pyq)
-
-# if len(unsuccessful) > 0:
-#    print("------------Unsuccessful Items------------")
-#    for item in unsuccessful:
-#        print(item)
-# else:
-#    print("------------Successfully wrote all data into file------------")
-
-# print("Merging CSVs...")
-# merge_csv.merge("combined.csv", filename)
